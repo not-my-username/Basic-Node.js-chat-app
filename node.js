15ed9499a5bfa03033953c8e42b46292f4a9cdf1
NOT-MY-USER-NAME <95655127+NOT-MY-USER-NAME@users.noreply.github.com>
2022-01-09 15:54:28 +1300

    Update node.js

--- node.js
+++ node.js
@@ -4,7 +4,6 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const fs = require("fs");
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,7 +11,6 @@ io.on("connection", (socket) => {
     socket.on("message", function(data) {
         console.log(data);
         socket.broadcast.emit("message", data);
-        fs.appendFileSync("./public/history.txt", data.username + ": " + data.message + "<br/><br/>");
     });
 });
 
